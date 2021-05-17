84e98eb966c5fb71bcf386655938f0fc9d6c6476
bgdtomas <71855224+bgdtomas@users.noreply.github.com>
2021-05-17 14:17:32 -0300

    Añadido el modelo de post

---
+++ Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alkemy_blog_challenge.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public string Imagen { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
