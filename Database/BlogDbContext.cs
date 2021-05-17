d0ced77c33663e1fe994baad78b7d0782b5a85aa
bgdtomas <71855224+bgdtomas@users.noreply.github.com>
2021-05-17 15:09:27 -0300

    Restricciones al modelo

---
+++ Database/BlogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alkemy_blog_challenge.Database
+{
+    public class BlogDbContext : DbContext 
+    {
+    }
+}
--- Models/Post.cs
+++ Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,30 @@ namespace alkemy_blog_challenge.Models
 {
     public class Post
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "El titulo es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres.")]
+        [Display(Name = "Titulo")]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "No se puede subir un post vacio.")]
+        [MaxLength(100, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres.")]
+        [Display(Name = "Contenido")]
         public string Contenido { get; set; }
-        public string Imagen { get; set; }
+
+        [Display(Name = "Imagen")]
+        public byte[] Imagen { get; set; }
+
+        [Required(ErrorMessage = "Debes definir una categoria")]
+        [MaxLength(50, ErrorMessage = "El {0} debe ser al menos {2} y maximo {1} caracteres.")]
+        [Display(Name = "Categoria")]
         public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de creacion.")]
         public DateTime FechaCreacion { get; set; }
     }
 }
