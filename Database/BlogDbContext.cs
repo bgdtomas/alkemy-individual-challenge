b1c3b5962f515ecec4966b3cc52089d334a0122a
bgdtomas <71855224+bgdtomas@users.noreply.github.com>
2021-05-18 09:49:57 -0300

    Terminada la creacion de la base de datos

--- Database/BlogDbContext.cs
+++ Database/BlogDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using alkemy_blog_challenge.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,11 @@ namespace alkemy_blog_challenge.Database
 {
     public class BlogDbContext : DbContext 
     {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Post { get; set; }
     }
 }
