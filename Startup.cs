60e2eac987cd5b6d28d46fb0e287d0c5598af239
bgdtomas <71855224+bgdtomas@users.noreply.github.com>
2021-05-19 13:36:29 -0300

    Posts/index set as default routing

--- Startup.cs
+++ Startup.cs
@@ -51,7 +51,7 @@ namespace alkemy_blog_challenge
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Posts}/{action=Index}/{id?}");
             });
         }
     }
