713d3678a52d36c42b1c4a5c5ef6a8ee0674174d
bgdtomas <71855224+bgdtomas@users.noreply.github.com>
2021-05-20 11:04:32 -0300

    created ISoftDelete interface

---
+++ Models/ISoftDelete.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alkemy_blog_challenge.Models
+{
+    interface ISoftDelete
+    {
+    }
+}
